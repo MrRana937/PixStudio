5525e6bd30c2c5cb4d4e923ea344472c43afda2c
vishal rana <mrjohn7861@gmail.com>
2025-02-22 15:40:29 +0530

    Integrated onchange for every shape all adding shapes to the canvas on click

--- src/app/features/editor/types.ts
+++ src/app/features/editor/types.ts
@@ -1,3 +1,4 @@
+import {fabric} from "fabric";
 
 export type ActiveTool =
   | 'select'
@@ -15,3 +16,62 @@ export type ActiveTool =
   | 'ai'
   | 'remove-bg'
   | 'templates';
+
+  
+  export const FILL_COLOR="rgba('0,0,0,1')"
+  export const STROKE_COLOR = "rgba('0,0,0,1')"
+  export const STROKE_WIDTH =2
+
+  export const CIRCLE_OPTIONS = {
+    height: 400,
+    width: 400,
+    radius: 225,
+    fill: FILL_COLOR,
+    stroke: STROKE_COLOR,
+    strokeWidth:STROKE_WIDTH
+  }
+
+  export const RECTANGLE_OPTIONS = {
+    height: 400,
+    width: 400,
+    left:100,
+    top:100,
+    fill: FILL_COLOR,
+    stroke: STROKE_COLOR,
+    strokeWidth: STROKE_WIDTH,
+    angle:0
+  }
+ export const TRIANGLE_OPTIONS = {
+   height: 400,
+   width: 400,
+   left: 100,
+   top: 100,
+   fill: FILL_COLOR,
+   stroke: STROKE_COLOR,
+   strokeWidth: STROKE_WIDTH,
+   angle: 0,
+ }
+ export const DIAMOND_OPTIONS = {
+   height: 600,
+   width: 600,
+   left: 100,
+   top: 100,
+   fill: FILL_COLOR,
+   stroke: STROKE_COLOR,
+   strokeWidth: STROKE_WIDTH,
+   angle: 0,
+ }
+
+  export interface Editor {
+    addCircle: () => void
+    addSoftRectangle: () => void
+    addRectangle: () => void
+    addTriangle: () => void
+    addInverseTriangle:()=>void
+    addDiamond:()=>void
+  }
+
+
+  export type BuildEditorProps={
+  canvas:fabric.Canvas
+  }
