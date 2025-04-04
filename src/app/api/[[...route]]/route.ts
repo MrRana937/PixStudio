ce5c48f9e3ae38b4458235483ece6a91072c7873
vishal rana <mrjohn7861@gmail.com>
2025-04-04 13:57:54 +0530

    hono testing integration and testing with application for seperation

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,21 +1,18 @@
 import {Hono} from 'hono';
 import {handle} from 'hono/vercel'
 import user from './user'
-
 export const runtime ="nodejs"
 
 
 
 const app = new Hono().basePath('/api');
 
-app.get('/test',(c)=>{
-    return c.json({hono:"testing the hono api "})
-})
 
-//groupoing routes for large production aplication we use grouping 
-app.route("/user",user)
+const routes=app.route("/user",user)
 
 
 //as route is reserved keyword and its expect either get post put delte 
 //so we have to wrap it inorder to work 
 export const GET=handle(app);
+
+export type AppType= typeof routes;
--- src/app/api/[[...route]]/user.ts
+++ src/app/api/[[...route]]/user.ts
@@ -3,18 +3,23 @@ import {z} from "zod"
 import {zValidator} from "@hono/zod-validator"
 
 const app= new Hono()
-
-app.get('/',(c)=>{
+.get('/',(c)=>{
     return c.json({user:"get"})
 
 })
-
-app.get('/:name',zValidator("param",z.object({
+.get('/:name',zValidator("param",z.object({
     name:z.number()
 })),(c)=>{
 
     const params=c.req.valid("param");
+
+    if(true)
+        {
+           return  c.json({error:"something went wrong "},401)
+        }
+        
     return c.json({username:params.name})
 })
+
 
 export default app
